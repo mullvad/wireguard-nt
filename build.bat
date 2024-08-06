fa4acc9c0432ad9a8f0a72d79a84dc47779524a9
David Lönnhager <david.l@mullvad.net>
2024-08-06 11:57:35 +0200

    Fix /os argument on ARM64 in build.bat

--- build.bat
+++ build.bat
@@ -14,6 +14,11 @@ if "%OUT_DIR_ARCH%"=="x64" (
   set OUT_DIR_ARCH=amd64
 )
 
+set INF2CAT_OS=10_%VSCMD_ARG_TGT_ARCH%
+if "%VSCMD_ARG_TGT_ARCH%"=="arm64" (
+  set INF2CAT_OS=10_VB_%VSCMD_ARG_TGT_ARCH%
+)
+
 set ROOT=%~dp0
 
 rmdir /s /q %ROOT%Release
@@ -31,7 +36,7 @@ IF %ERRORLEVEL% NEQ 0 goto ERROR
 :: Re-generate catalog file now that driver binary has changed
 
 del %ROOT%Release\%OUT_DIR_ARCH%\driver\mullvad-wireguard.cat
-"%WindowsSdkVerBinPath%x86\inf2cat.exe" /driver:%ROOT%Release\%OUT_DIR_ARCH%\driver /os:"10_%VSCMD_ARG_TGT_ARCH%" /verbose
+"%WindowsSdkVerBinPath%x86\inf2cat.exe" /driver:"%ROOT%Release\%OUT_DIR_ARCH%\driver" /os:"%INF2CAT_OS%" /verbose
 
 IF %ERRORLEVEL% NEQ 0 goto ERROR
 
